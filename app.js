88e86f239826756eb214ab10b7e5a53e636c0aba
Mehmetcan Ece <mehmetcanece02@hotmail.com>
2024-11-28 12:04:41 +0300

    url, method ve headers loglandı. process exit ile server kapatıldı

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const http = require("http");
 
-http.createServer(function (req, res) {}); // Server object
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers); // url / olarak dönüyor çünkü sadece local host 3000 var, methodumuz GET, headers ise objeyi dönüyor.
+  /*   process.exit(); //bunu yazadığında server kapanıyor, hard exit.
+   */
+});
+
+server.listen(3000);
