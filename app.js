4a76b55bd0c89976349e3139e2f758f16a3b8f97
Mehmetcan Ece <mehmetcanece02@hotmail.com>
2024-12-04 14:06:19 +0300

    refactor

--- app.js
+++ app.js
@@ -5,38 +5,35 @@ const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
 
+  console.log(`Request received for: ${url}`);
+
   if (url === "/") {
+    console.log("Serving homepage");
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write(
-<<<<<<< HEAD
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
-=======
       '<body><form action="/message" method="POST"><input name="message" type="text"><button type="submit">Gonder</button></form></body>'
->>>>>>> servers
     );
     res.write("</html>");
     return res.end();
   }
+
   if (url === "/message" && method === "POST") {
-<<<<<<< HEAD
-    fs.writeFileSync("message.txt", "DUMMY");
-=======
-    fs.writeFileSync("message.txt", "DUMMY"); // bu kodu yazdığında server kapanıyor, hard exit.
->>>>>>> servers
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
+    console.log("Form submitted");
+    fs.writeFileSync("message.txt", "DUMMY"); // Dosyaya yazma işlemi
+    res.statusCode = 302; // Yönlendirme kodu
+    res.setHeader("Location", "/"); // Anasayfaya yönlendirme
     return res.end();
   }
-<<<<<<< HEAD
-  console.log("App is running");
-=======
 
->>>>>>> servers
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
   res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
   res.write("</html>");
   res.end();
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000...");
 });
