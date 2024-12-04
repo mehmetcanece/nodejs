8dd1778d87de33d45607eeb034cd553870e98faa
Mehmetcan Ece <mehmetcanece02@hotmail.com>
2024-12-04 11:28:16 +0300

    before redirecting request

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers); // url / olarak dönüyor çünkü sadece local host 3000 var, methodumuz GET, headers ise objeyi dönüyor.
+  //console.log(req.url, req.method, req.headers); // url / olarak dönüyor çünkü sadece local host 3000 var, methodumuz GET, headers ise objeyi dönüyor.
+
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>"); // responsa yazdığımız şeyler
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input name="message" type="text"><button type="submit"></button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+  }
   /*   process.exit(); //bunu yazadığında server kapanıyor, hard exit.
    */
   res.setHeader("Content-Type", "text/html");
   res.write("<html>"); // responsa yazdığımız şeyler
-  res.write("<head><title>İlk Sayfam</title></head>");
+  res.write("<head><title>My First Page</title></head>");
   res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
   res.write("</html>");
-  //res.end(); buraya bir şey yazamayız çünkü res.end() dan sonra çalışmaz.
+  return res.end();
 });
 
 server.listen(3000);
